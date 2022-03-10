6eda11e594a8dc9cf0ec9ad15f431e172a525db9
D.mon 24 <dchaminov@ya.ru>
2022-03-10 08:30:06 +0300

    New alements markup (for saving and graph colors params)

--- coursework/resource.h
+++ coursework/resource.h
@@ -28,6 +28,9 @@
 #define IDC_EDIT_xscale_to              1037
 #define IDC_EDIT_yscale_to              1038
 #define IDC_SLIDER_STEP                 1039
+#define IDC_BUTTON1                     1040
+#define IDC_BUTTON_SAVE_GR              1040
+#define IDC_BUTTON_SETTINGS_GR          1041
 
 // Next default values for new objects
 // 
@@ -35,7 +38,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        136
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1040
+#define _APS_NEXT_CONTROL_VALUE         1042
 #define _APS_NEXT_SYMED_VALUE           106
 #endif
 #endif
