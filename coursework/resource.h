2c729344daeaa46e4e70ccbb72789cfd5f6a8d99
D.mon 24 <dchaminov@ya.ru>
2022-03-10 18:30:23 +0300

    new wnd added

--- coursework/resource.h
+++ coursework/resource.h
@@ -10,6 +10,7 @@
 #define IDD_MyTaskAboutDlg              103
 #define IDD_Calculator                  104
 #define IDD_DataErrorDlg                105
+#define IDD_ColorSettingsDLG            106
 #define IDR_MAINFRAME                   128
 #define ID_LOGO_ICO                     133
 #define IDC_STATIC_TASK                 1000
@@ -31,14 +32,15 @@
 #define IDC_BUTTON1                     1040
 #define IDC_BUTTON_SAVE_GR              1040
 #define IDC_BUTTON_SETTINGS_GR          1041
+#define IDC_STATIC_grad                 1042
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1042
-#define _APS_NEXT_SYMED_VALUE           106
+#define _APS_NEXT_CONTROL_VALUE         1043
+#define _APS_NEXT_SYMED_VALUE           107
 #endif
 #endif
