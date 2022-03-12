1b2cf45462fafcc6d5c08285e238c4e2bb34d2bd
D.mon 24 <dchaminov@ya.ru>
2022-03-13 00:20:11 +0300

    Revert "Merge pull request #4 from Dimach24/revert-3-other-functions-system"
    This reverts commit 4faa7dc1f43647ba78c8c3a2b2c3d4ff836e657d, reversing
    changes made to 4f33726076cffd7c7dfc850e4b1718aaa59ebe0d.

--- coursework/Calculator.cpp
+++ coursework/Calculator.cpp
@@ -13,9 +13,10 @@ IMPLEMENT_DYNAMIC(Calculator, CDialogEx)
 
 Calculator::Calculator(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_Calculator, pParent),
-	signal(0, 0, 0)
+	signal(),
+	dft(nullptr)
 {
-
+	dft.set_signal(&signal);
 }
 
 Calculator::~Calculator()
@@ -28,7 +29,7 @@ BOOL Calculator::OnInitDialog()
 	axes_cp.SubclassDlgItem(IDC_MFCCOLORBUTTON_AXES, this);
 	bg_cp.SubclassDlgItem(IDC_MFCCOLORBUTTON_BG, this);
 	signal_cp.SubclassDlgItem(IDC_MFCCOLORBUTTON_SIGNAL, this);
-	dcf_cp.SubclassDlgItem(IDC_MFCCOLORBUTTON_DCF, this);
+	dft_cp.SubclassDlgItem(IDC_MFCCOLORBUTTON_DCF, this);
 	slider_step.SubclassDlgItem(IDC_SLIDER_STEP, this);
 	cb_is_log.SubclassDlgItem(IDC_CHECK_is_log_scale, this);
 
@@ -38,29 +39,20 @@ BOOL Calculator::OnInitDialog()
 
 	CWnd* p = GetDlgItem(IDC_STATIC_signal);
 	if (p) { p->SetWindowTextW(_T("x(t) = a*sin(2\u03c0(f + mt)*t")); }
-
+	p = nullptr;
 	p = GetDlgItem(IDC_EDIT_param_a);
 	if (p) { p->SetFocus(); }
 
-	p = GetDlgItem(IDC_EDIT_xscale_from);
-	if (p) { p->SetWindowTextW(L"-6.2832"); }
-	p = GetDlgItem(IDC_EDIT_xscale_to);
-	if (p) { p->SetWindowTextW(L"6.2832"); }
-	p = GetDlgItem(IDC_EDIT_yscale_from);
-	if (p) { p->SetWindowTextW(L"-1.5"); }
-	p = GetDlgItem(IDC_EDIT_yscale_to);
-	if (p) { p->SetWindowTextW(L"1.5"); }
-	
-	bg_cp.SetColor(RGB(0xff, 0xfb, 0xf0));
-	signal_cp.SetColor(RGB(0x80, 0, 0));
-	axes_cp.SetColor(RGB(0, 0, 0));
-	dcf_cp.SetColor(RGB(0, 0, 0x80));
+	ResetInputData();
+	ResetColorPickers();
 
-	MathFunction f;
-	f.set_function(&signal);
-	f.set_definition_scope(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
-	f.set_color(RGB(0, 0, 0));
-	Cgraph.functions.push_back(f);
+	signal.set_definition_scope(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
+	Cgraph.functions.push_back(&signal);
+	
+	dft.set_definition_scope(-std::numeric_limits<double>::infinity(), std::numeric_limits<double>::infinity());
+	Cgraph.functions.push_back(&dft);
+
+
 	return 0;
 }
 #define MY_PARAM_HELPER(P,ID,NAME)\
@@ -81,12 +73,9 @@ void Calculator::UpdateCalculatorParams()
 	MY_PARAM_HELPER(p, IDC_EDIT_yscale_from, y_from);
 	MY_PARAM_HELPER(p, IDC_EDIT_yscale_to, y_to);
 	if (a_s != "" && m_s != "" && f_s != "" && x_from_s != "" && x_to_s != "" && y_from_s != "" && y_to_s != "") {
-		if (m != signal.m || a != signal.a || f != signal.f) {
-			Cgraph.setNotCalculated();
-			signal.m = m;
-			signal.a = a;
-			signal.f = f;
-		}
+		signal.set_a(a);
+		signal.set_f(f);
+		signal.set_m(m);
 		Cgraph.setScale(x_from, x_to, y_from, y_to);
 		RECT r;
 		Cgraph.GetWindowRect(&r);
@@ -99,7 +88,8 @@ void Calculator::UpdateCalculatorParams()
 		Cgraph.setLog(cb_is_log.GetCheck() == 1);
 		Cgraph.axes_color = axes_cp.GetColor();
 		Cgraph.bg_color = bg_cp.GetColor();
-		Cgraph.functions[0].color = signal_cp.GetColor();
+		Cgraph.functions[0]->color = signal_cp.GetColor();
+		Cgraph.functions[1]->color = dft_cp.GetColor();
 		p = GetDlgItem(IDC_STATIC_signal);
 		if (p) {
 			CString signal;
@@ -129,6 +119,7 @@ void Calculator::DoDataExchange(CDataExchange* pDX)
 
 BEGIN_MESSAGE_MAP(Calculator, CDialogEx)
 	ON_BN_CLICKED(IDC_BUTTON_UPDATE_graph, &Calculator::OnBnClickedButtonUpdate)
+	ON_BN_CLICKED(IDC_BUTTON_reset, &Calculator::OnBnClickedButtonreset)
 END_MESSAGE_MAP()
 
 
@@ -139,4 +130,46 @@ void Calculator::OnBnClickedButtonUpdate()
 {
 	UpdateCalculatorParams();
 	Cgraph.RedrawWindow();
+}
+
+
+void Calculator::OnBnClickedButtonreset()
+{
+	ResetInputData();
+
+	ResetColorPickers();
+}
+
+void Calculator::ResetColorPickers()
+{
+	bg_cp.SetColor(RGB(0xff, 0xfb, 0xf0));
+	signal_cp.SetColor(RGB(0x80, 0, 0));
+	axes_cp.SetColor(RGB(0, 0, 0));
+	dft_cp.SetColor(RGB(0, 0, 0x80));
+}
+
+void Calculator::ResetInputData()
+{
+	CWnd* p;
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_param_a);
+	if (p) { p->SetWindowTextW(L"1"); }
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_param_m);
+	if (p) { p->SetWindowTextW(L"0"); }
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_param_f);
+	if (p) { p->SetWindowTextW(L"0.1592"); }
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_xscale_from);
+	if (p) { p->SetWindowTextW(L"-6.2832"); }
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_xscale_to);
+	if (p) { p->SetWindowTextW(L"6.2832"); }
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_yscale_from);
+	if (p) { p->SetWindowTextW(L"-1.5"); }
+	p = nullptr;
+	p = GetDlgItem(IDC_EDIT_yscale_to);
+	if (p) { p->SetWindowTextW(L"1.5"); }
 }
--- coursework/Calculator.h
+++ coursework/Calculator.h
@@ -12,6 +12,9 @@ public:
 	virtual ~Calculator();
 	BOOL OnInitDialog();
 	void UpdateCalculatorParams();
+protected:
+	SignalFunction signal;
+	DFTFunction dft;
 // Данные диалогового окна
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_Calculator };
@@ -25,9 +28,12 @@ public:
 	afx_msg void OnBnClickedButtonUpdate();
 	CSliderCtrl slider_step;
 	CButton cb_is_log;
-	SignalFunction signal;
 	CMFCColorButton axes_cp;
 	CMFCColorButton bg_cp;
 	CMFCColorButton signal_cp;
-	CMFCColorButton dcf_cp;
+	CMFCColorButton dft_cp;
+	afx_msg void OnBnClickedButtonreset();
+protected:
+	void ResetColorPickers();
+	void ResetInputData();
 };
