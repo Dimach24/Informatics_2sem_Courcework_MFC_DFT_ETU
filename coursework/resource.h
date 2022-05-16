43eddcac435173307eba46b30983a3e52f62f907
D.mon 24 <dchaminov@ya.ru>
2022-05-16 08:50:27 +0300

    redesigned

--- coursework/resource.h
+++ coursework/resource.h
@@ -47,7 +47,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        138
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1049
+#define _APS_NEXT_CONTROL_VALUE         1055
 #define _APS_NEXT_SYMED_VALUE           107
 #endif
 #endif
